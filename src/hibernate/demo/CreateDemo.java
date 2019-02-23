a73cb957f6535244de2ecfc4bc6e15d379b3472a
Oleksandr <sashko.kondratenko@gmail.com>
2019-02-23 02:53:14 +0100

    OneToOne uni

---
+++ src/hibernate/demo/CreateDemo.java
@@ -0,0 +1,44 @@
+package hibernate.demo;
+
+import hibernate.entity.Instructor;
+import hibernate.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                                .configure("hibernate.cfg.xml")
+                                .addAnnotatedClass(Instructor.class)
+                                .addAnnotatedClass(InstructorDetail.class)
+                                .buildSessionFactory();
+        // create a session
+        Session session = factory.getCurrentSession();
+
+        try {
+            //create the objects
+            Instructor tempInstructor = new Instructor("Will", "Smith", "smith@gmail.com");
+
+            InstructorDetail tempDetail = new InstructorDetail("smith/youtube", "fishing");
+
+            // assosiate the objects
+            tempInstructor.setInstructorDetail(tempDetail);
+
+            //start a transaction
+            session.beginTransaction();
+
+            // save the instructor
+            session.save(tempInstructor);
+
+            // commit transaction
+            session.getTransaction().commit();
+        }
+        finally {
+            factory.close();
+        }
+    }
+}
